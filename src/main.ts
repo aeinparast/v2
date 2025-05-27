d6dfc3465d7d9b6ca3e3efd2f4771e83e3a449b4
Rayan <akhbargameap@gmail.com>
2025-05-27 20:03:26 +0330

    select file type

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,14 @@ import 'aos/dist/aos.css';
 
 AOS.init();
 
+
+
+
+const selectedRadio = document.querySelector(
+    '.mainSelectors__new--items--input:checked'
+) as HTMLInputElement;
+
+if (selectedRadio) {
+    console.log("Selected value:", selectedRadio.value); // "all", "land", etc.
+    console.log("Selected ID:", selectedRadio.id);      // "mainSelectors__new--all", etc.
+}
